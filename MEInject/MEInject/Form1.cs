54b675818cae948fd3a330436221e8d30fcb2966
kolyandex <iam@kolyandex.su>
2017-03-27 19:39:51 +0700

    beta3

--- MEInject/MEInject/Form1.cs
+++ MEInject/MEInject/Form1.cs
@@ -19,11 +19,20 @@ namespace MEInject
         private int BIOS_ME_offset;
         private string BIOSfilename;
         private static byte _diff = 0x10;
+        private Mode _mode;
+
+        enum Mode
+        {
+            ME,
+            TXE
+        }
+
         public Form1()
         {
             Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
             InitializeComponent();
             Log("Ready!");
+            OpenMEButton.Enabled = false;
         }
 
 
@@ -33,28 +42,41 @@ namespace MEInject
             var ofd = new OpenFileDialog { Multiselect = false };
             if (ofd.ShowDialog() != DialogResult.OK) return;
             BIOStextbox.Text = "";
+            MEtextbox.Text = "";
+            MEfile = null;
             BIOSfile = File.ReadAllBytes(ofd.FileName);
             var offset = Find(BIOSfile, Mask);
-            //MessageBox.Show(offset1.ToString("X") + "<-mask1 mask2->" + (offset2 - 0x10).ToString("X"));
+
             if (offset == -1)
             {
-                Log("Can't find ME region in file");
+                Log("Can't find ME/TXE region in file");
                 BIOSfile = null;
+                OpenMEButton.Enabled = false;
                 return;
             }
             offset -= _diff;
             if (offset == 0)
             {
-                Log("Please, open BIOS, not ME");
+                Log("Please, open BIOS, not ME/TXE");
                 BIOSfile = null;
+                OpenMEButton.Enabled = false;
                 return;
             }
+           
+
             BIOSfilename = ofd.SafeFileName;
             BIOS_ME_offset = offset;
-            MEoffsetLabel.Text = "ME offset in BIOS: 0x" + BIOS_ME_offset.ToString("X8");
+            _mode = BIOSfile[offset] == 0x00 ? Mode.TXE : Mode.ME;
+
+            Log("Mode: " + _mode);
+
+            OpenMEButton.Enabled = BIOSfile != null;
+            OpenMEButton.Text = "Open " + _mode;
+
+            MEoffsetLabel.Text = _mode + " offset in BIOS: 0x" + BIOS_ME_offset.ToString("X8");
             BIOSsizeLabel.Text = "BIOS size: 0x" + BIOSfile.Length.ToString("X8");
             Log("BIOS read successful! " + ofd.SafeFileName);
-            BIOStextbox.Text = "First 0x10 bytes ME in BIOS:\n";
+            BIOStextbox.Text = "First 0x10 bytes " + _mode + " in BIOS:\n";
             for (int i = 0; i < 32; i++)
             {
                 if (i % 8 == 0) BIOStextbox.Text += "\n";
@@ -118,19 +140,34 @@ namespace MEInject
             MEfile = File.ReadAllBytes(ofd.FileName);
             if (Find(MEfile, Mask) - _diff != 0)
             {
-                Log("It's not valid ME file");
+                Log("It's not valid " + _mode + " file");
                 MEfile = null;
                 return;
             }
-            MEtextbox.Text = "First 0x10 bytes in ME:\n";
+
+            if ((MEfile[0] == 0x00 && _mode == Mode.ME) || (MEfile[0] != 0x00 && _mode == Mode.TXE))
+            {
+                Log("Please, open " + _mode + " file");
+                MEfile = null;
+                return;
+            }
+
+            if (MEfile.Length >= BIOSfile.Length)
+            {
+                Log(_mode + " file is larger then BIOS file!");
+                MEfile = null;
+                return;
+            }
+
+            MEtextbox.Text = "First 0x10 bytes in " + _mode + ":\n";
             for (int i = 0; i < 32; i++)
             {
                 if (i % 8 == 0) MEtextbox.Text += "\n";
                 if (i % 4 == 0) MEtextbox.Text += " ";
                 MEtextbox.Text += MEfile[i].ToString("X2") + " ";
             }
-            MEsizeLabel.Text ="ME size: 0x" + MEfile.Length.ToString("X8");
-            Log("ME read successful! " + ofd.SafeFileName);
+            MEsizeLabel.Text = _mode + " size: 0x" + MEfile.Length.ToString("X8");
+            Log(_mode + " read successful! " + ofd.SafeFileName);
         }
 
         void Log(string message)
